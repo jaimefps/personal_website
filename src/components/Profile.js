c4217fe249e50e540a8982af2df28ecc823b8c17
Jaime Pericas Saez <jaimefps@Jaimes-MacBook-Pro.local>
2017-06-05 12:57:19 -0700

    prof on left

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,17 +12,6 @@ class Profile extends Component {
   render() {
     return (
       <div className="wrap">
-
-
-      <div className="pixels-container">
-            <div className='description'> Programmer by day. Artist & Philosopher by night. </div>
-            <div className="imgs-container">
-              <img className='pixel-img' src={hacker} />
-              <img className='pixel-img' src={artist} />
-              <img className='pixel-img' src={philosopher} />
-            </div>
-      </div>
-
       <div className="profile-container">
             <div className="profile-info">
               <div className="name"> Jaime Pericás Sáez </div>
@@ -36,6 +25,14 @@ class Profile extends Component {
               <div><a target='_blank' href='https://github.com/jaimefps'> GitHub </a></div>
               <div><a target='_blank' href='https://www.linkedin.com/in/jaime-pericas-saez/'>  LinkedIn </a></div>
               <div><a target='_blank' href='https://docs.google.com/document/d/1bFQezsn2kNOR8GXDXdSPuME2LK4F7yWD0nseItrXals/edit?usp=sharing'> Resume </a></div>
+            </div>
+      </div>
+      <div className="pixels-container">
+            <div className='description'> Programmer by day. Artist & Philosopher by night. </div>
+            <div className="imgs-container">
+              <img className='pixel-img' src={hacker} />
+              <img className='pixel-img' src={artist} />
+              <img className='pixel-img' src={philosopher} />
             </div>
       </div>
       </div>
